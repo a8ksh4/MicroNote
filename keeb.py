0d726f56b9421c78fd59b92c70bcf98958cdc4f8
Dan Norris <norris.daniel@gmail.com>
2022-07-03 18:26:30 -0700

    progress on keeb loop

--- keeb.py
+++ keeb.py
@@ -1,73 +1,92 @@
 from machine import Pin
 import time
 from keymap import LAYERS, CHORDS
+import collections
+
+EVENT_T = collections.namedtuple('buttons', 
+                                'start_time', 
+                                'output_key',
+                                'last_output_time',
+                                'layer')
+
 
 # https://docs.arduino.cc/tutorials/nano-rp2040-connect/rp2040-python-api#gpio-map
 rp2040_pins = [4, 7, 5, 21, 20, 19, 18, 17, 16, 15]
 pins = [Pin(n, Pin.IN, Pin.PULL_UP) for n in rp2040_pins]
 
 #HOLD_TIME = 500
-LAYER_HOLDTIME = 300
-CHORD_WAITTIME = 500
+#LAYER_HOLDTIME = 300
+#CHORD_WAITTIME = 500
+HOLDTIME = 500
 ONESHOT_TIMEOUT = 2000
 BASE_LAYER = 0
 TICKER = 0
+EVENTS = []
+PENDING_BUTTONS = set()
+# (keys, start_time, output key, last_output_time)
+# (pins, start_time, output_key, last_output_time)
 
 # Pin map to buttons:
 #  1  3  5  7  9
 #  0  2  4  6  8
 
-# while True:
-#     print([pin.value() for pin in pins])
-#     # for n, pin in enumerate(pins):
-#     #     print(pin.value())
-#     time.sleep(1)
+
 # Strategery
 # First keypress starts the process
 # Passing layer hold time transitions layer if it's a layer key
 #    layer transition stays until layer key is released
 # chord time starts when first key is pressed (or reset when layer shift is triggered)
 # 
-def get_pressed():
+def get_output_key(buttons, layer):
     pass
 
 def get_next():
     global TICKER
-    state = 'fresh'
-    last_active = []
-    ticker = 0
-    layer = BASE_LAYER
+    global HOLDTIME
+    global BASE_LAYER
+    global EVENTS
+    global PENDING_BUTTONS
+
     while True:
         time.sleep(0.001)
         clock = time.ticks_ms()
-        active_buttons = [n for n, p  in enumerate(pins) if p.value()]
 
-        # Idle check
-        if not active_buttons and not last_active:
-            continue
+        current_layer = [BASE_LAYER,] + [e.layer for e in EVENTS if e.layer]
+        current_layer = current_layer[-1]
 
-        # Wait for end of action button release
-        if ticker == -1:
-            if active_buttons:
-                continue
-            else:
-                ticker = 0
-                last_active = active_buttons
+        buttons_pressed = [n for n, p  in enumerate(pins) if p.value()]
+        new_event = False
 
-        # Start something
-        if active_buttons and not last_active:
-            ticker = clock
-            last_active = active_buttons
-            continue
+        # Remove events whos buttons are no longer pressed.
+        for event in EVENTS:
+            still_pressed = [b for b in event.buttons if b in buttons_pressed]
+            if not still_pressed:
+                EVENTS.remove(event)
 
-        # Check HOLDTIME
-        if clock - ticker > CHORD_WAITTIME:
-            pressed = get_pressed(active_buttons if active_buttons else last_active)
-            ticker = -1 # block until non-layer keys released
-            return pressed
+        # Remove buttons from buttons_pressed if associated with an event
+        all_event_butons = sum([e.buttons for e in EVENTS])
+        buttons_pressed = [b for b in buttons_pressed if b not in all_event_butons]
+        # TODO: mark events inactive if not all of their original buttons are still pressed.
 
-        # if not active_buttons:
-        # if len(active_buttons) > last_active:
-        #     last_active
-        # # Continued Press
-        # if len(last_active) ==
+        # Move buttons_pressed to pending
+        PENDING_BUTTONS.update(buttons_pressed)
+        # At this point, pending buttons is only buttons pressed that have not been associated with an event. 
+
+        if PENDING_BUTTONS:
+            if not TICKER:
+                TICKER = clock
+
+            # Check conditions for new event - Keys starting to be released or hold_time exceeded
+            # TODO: maybe separate hold times for layer stuff vs chords. 
+            if ( len(PENDING_BUTTONS) > len(buttons_pressed) 
+                    or clock - TICKER > HOLDTIME ):
+                output_key, new_layer = get_output_key(buttons, curernt_layer)
+                new_event = EVENT_T(buttons=list(PENDING_BUTTONS),
+                                    start_time=clock,
+                                    output_key=output_key,
+                                    last_output_time=0,
+                                    layer=new_layer)
+
+        if EVENTS:
+            last_event = EVENTS[-1]
+            return_key = last_event.outpu
